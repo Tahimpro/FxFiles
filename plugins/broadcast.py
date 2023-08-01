ab4ed28f0ead30361dceba85d97dc3e04b3f82dc
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2023-08-02 01:13:34 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -12,21 +12,7 @@ import asyncio
 async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    forwarded_text = None
-
-    if message.reply_to_message:
-        if message.reply_to_message.is_forwarded:
-            header = f"Forwarded from {message.reply_to_message.forward_from_chat.title} ({message.reply_to_message.forward_from_chat.username})\n"
-            forwarded_text = header + (message.reply_to_message.text or "Message contains media or other content.")
-        else:
-            header = f"Forwarded from {message.reply_to_message.from_user.full_name} ({message.reply_to_message.from_user.username})\n"
-            forwarded_text = header + (message.reply_to_message.text or "Message contains media or other content.")
-
-    if not forwarded_text:
-        await message.reply_text("Error: You need to reply to a message to broadcast it.")
-        
-        return
-sts = await message.reply_text(
+    sts = await message.reply_text(
         text='Broadcasting your messages...'
     )
     start_time = time.time()
@@ -34,7 +20,7 @@ sts = await message.reply_text(
     done = 0
     blocked = 0
     deleted = 0
-    failed = 0
+    failed =0
 
     success = 0
     async for user in users:
